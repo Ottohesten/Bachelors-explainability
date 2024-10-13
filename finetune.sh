d554b5d7eb1df64f3ba05e57510e27a05f957a87
Otto Schmidt <s194101@titans.compute.dtu.dk>
2024-10-13 22:54:18 +0200

    added finetune to repo

---
+++ finetune.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=mmidb_noica_noica_5.0
+#SBATCH --output=/home/s194101/Bachelors-explainability/experiment_logs/mmidb_noica_noica_5.0-%J.out
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=12gb
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=otto@skytop.dk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=titans
+#SBATCH --export=ALL
+#SBATCH --time=04:00:00
+
+## INFO
+echo "Node: $(hostname)"
+echo "Start: $(date +%F-%R:%S)"
+echo -e "Working dir: $(pwd)\n"
+
+source ~/.bashrc
+source activate myenv
+python scripts/finetune_cv.py --config configs/finetune/mmidb_noica_noica_5.0.yaml
+
+echo "Done: $(date +%F-%R:%S)
